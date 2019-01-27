80858db92ee51939b9be1cc28cb0124b75ab79ca
Matheus Homsi <mlhomsi@gmail.com>
2019-01-27 04:30:31 -0200

    Muita coisa, sorry...

---
+++ Assets/Scripts/AreaProcessingTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaProcessingTest : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject postprocessing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.transform.position.x > 55f)
+        {
+           // Debug.Log(player.transform.position.x);
+
+        }
+
+    }
+}
