1244644e848524ff9ac5cd501ce0f0f9c2ea2085
Joao Victor Galindo <doublito@gmail.com>
2019-01-27 13:27:04 -0200

    Add script de killzone e checkpoint para o Player e Ghost

---
+++ Assets/Scripts/KillChar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillChar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+    	if (col.gameObject.CompareTag("Player"))
+    	{
+    		Debug.Log("Era pra morrer");
+    		col.transform.position = col.gameObject.GetComponent<PlayerCheckpoint>().currentCheckpoint.transform.position;
+    	}
+
+    	if (col.gameObject.CompareTag("Ghost"))
+    	{
+    		col.transform.position = col.gameObject.GetComponent<GhostCheckpoint>().spawnPoint.transform.position;
+    	}
+    }
+
+}
