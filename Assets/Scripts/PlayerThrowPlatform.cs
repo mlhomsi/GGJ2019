749b0a88119c75c7b4421871539b01f73a1a79f6
Bernardo Hörner Lopes <behornerl@gmail.com>
2019-01-27 09:26:13 -0200

    Throwing platforms working

--- Assets/Scripts/PlayerThrowPlatform.cs
+++ Assets/Scripts/PlayerThrowPlatform.cs
@@ -9,7 +9,10 @@ public class PlayerThrowPlatform : MonoBehaviour
     private Vector2 myPos;
     private Quaternion rotation;
     private GameObject projectile;
+    private List<GameObject> platforms;
+    private GameObject currentPlatform;
 
+    public GameObject plat1, plat2, plat3;
     public GameObject seed_static;
     public GameObject seed_jumpPad;
 
@@ -19,6 +22,8 @@ public class PlayerThrowPlatform : MonoBehaviour
     void Start()
     {
         throwTransform = transform.GetChild(1);
+        platforms = new List<GameObject>();
+        platforms.Add(plat1); platforms.Add(plat2); platforms.Add(plat3);
     }
 
     void Update()
@@ -27,7 +32,27 @@ public class PlayerThrowPlatform : MonoBehaviour
         myPos = new Vector2(throwTransform.position.x, throwTransform.position.y);
         if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
         {
-            PrepareThrow();
+            /*Vector3 spawnPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+                Input.mousePosition.y, 10f));
+            Debug.Log(spawnPosition);
+
+            int platformNum = (int)Random.Range(0, 2);
+            GameObject currentPrefab = platforms[platformNum];
+            currentPlatform = Instantiate(currentPrefab, spawnPosition, transform.rotation);*/
+
+            // Enables the platform
+            if (hasThrown && currentPlatform == null)
+            {
+                int platformNum = (int)Random.Range(0, 2);
+                GameObject currentPrefab = platforms[platformNum];
+                currentPlatform = Instantiate(currentPrefab, projectile.transform.position, transform.rotation);
+                Destroy(projectile);
+                Invoke("SetHasThrownFalse", 1f);
+            }
+            else
+            {
+                PrepareThrow();
+            }
         }
         else if (Input.GetMouseButtonUp(0))
         {
@@ -39,15 +64,18 @@ public class PlayerThrowPlatform : MonoBehaviour
             hasThrown = false;
         }        
     }
-
+    private void SetHasThrownFalse()
+    {
+        hasThrown = false;
+    }
     // Saves the direction the player is aiming the seed
     Vector3 direction;
     private void PrepareThrow()
     {
-        Vector3 target = Camera.main.ScreenToWorldPoint(new Vector3
-            (Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+        direction = Camera.main.ScreenToWorldPoint(new Vector3
+            (Input.mousePosition.x, Input.mousePosition.y, 1));
         
-        direction = 10 * (target - Camera.main.transform.localPosition);
+        direction = (direction - Camera.main.transform.localPosition);
         Vector3.Normalize(direction);
         rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90);
     }
@@ -57,12 +85,13 @@ public class PlayerThrowPlatform : MonoBehaviour
     {
         if (!hasThrown)
         {
+            Destroy(currentPlatform);
             projectile = (GameObject)Instantiate(seed, myPos, rotation);
 
             // Stops the projectile so as to not get the player's speed
             projectile.GetComponent<Rigidbody2D>().velocity = new Vector3(0f, 0f, 0f);
             // Adds the expected projectile speed
-            projectile.GetComponent<Rigidbody2D>().velocity = direction * power;
+            projectile.GetComponent<Rigidbody2D>().velocity = direction.normalized * power;
             hasThrown = true;
         }
     }
