207995aa3d6398fefdc45b846e8b27bb13541919
Bernardo Hörner Lopes <behornerl@gmail.com>
2019-01-29 01:27:42 -0200

    De-fucking the project

---
+++ Assets/VictoryCondition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryCondition : MonoBehaviour
+{
+    public GameObject Win;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Ghost")
+        {
+            Debug.Log("WINN!!");
+            Win.SetActive(true);
+        }
+    }
+}
